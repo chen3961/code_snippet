93c5388818ad264c036599fc05f6927d3d85e89a
chen3961 <chen3961@qq.com>
2018-12-29 16:23:46 +0800

    Update file.py

--- python/file.py
+++ python/file.py
@@ -3,3 +3,6 @@ import os
 
 def get_file_name_without_extension(file_path):
   return os.path.splitext(os.path.basename(file_path))[0]
+
+def get_current_file_directory():
+  return os.path.dirname(os.path.realpath(__file__))
