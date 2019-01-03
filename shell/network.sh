67a558d0f0abcfec88a7ead952d2a9b7cfa9b463
chen3961 <chen3961@qq.com>
2019-01-03 20:10:56 +0800

    Update network.sh

--- shell/network.sh
+++ shell/network.sh
@@ -1,2 +1,6 @@
 # using proxy to wget file
 wget {url} -e use_proxy=yes -e http_proxy={ip}:{port} -e https_proxy={ip}:{port}
+
+# add service for firewalld
+firewall-cmd --add-service=dhcp --permanent
+firewall-cmd --reload
