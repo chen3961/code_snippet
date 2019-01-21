3ceffd385f5089a162963198177995445aa2860a
chen3961 <chen3961@qq.com>
2019-01-21 19:09:28 +0800

    add port forward

--- shell/network.sh
+++ shell/network.sh
@@ -4,3 +4,8 @@ wget {url} -e use_proxy=yes -e http_proxy={ip}:{port} -e https_proxy={ip}:{port}
 # add service for firewalld
 firewall-cmd --add-service=dhcp --permanent
 firewall-cmd --reload
+
+# add port forwarding
+iptables -t nat -A PREROUTING  -p tcp -d 1.1.1.1 --dport 80 -j DNAT --to-destination 2.2.2.2:8080
+iptables -t nat -A POSTROUTING -p tcp -s 2.2.2.2 --sport 8080 -j SNAT --to-source 1.1.1.1
+service iptables save
