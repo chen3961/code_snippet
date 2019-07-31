e5c1ac1461f95a56b0d01b4fde0e346325fdf52e
chen3961 <chen3961@qq.com>
2019-07-31 15:06:37 +0800

    Update ssl.sh

--- shell/ssl.sh
+++ shell/ssl.sh
@@ -3,4 +3,7 @@ openssl genrsa -out client.key
 
 # create a csr with private key
 # subj must be start with "/"
-openssl req -key ${client.key} -out ${client.csr} -subj "/CN=localhost"
+openssl req -new -key ${client.key} -out ${client.csr} -subj "/CN=localhost"
+
+# sign a csr with ca 
+openssl x509 -req -days 365 -in ${client.csr} -CA {ca.crt} -CAkey {ca.key} -out {client.crt} -CAcreateserial
