6703f2dc979b7f5456473a1422f866192a3e9f71
zchen <zhe.chen@yitu-inc.com>
2019-08-05 20:31:33 +0800

    add command for k8s

--- shell/k8s.sh
+++ shell/k8s.sh
@@ -1,2 +1,5 @@
 # copy object between namespace
 kubectl -n namesapcea get secrets/key -o json | jq '.namespace = "default"' | kubectl -n default create -f -
+
+# delete all object in namespace
+kubectl -n ${namespace} delete daemonsets,replicasets,services,deployments,pods,rc --all
