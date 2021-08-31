24385c65f45499d56a9f1d7dfc841015f75288d9
zchen <zhe.chen@yitu-inc.com>
2021-08-31 17:19:28 +0800

    Update

--- shell/k8s.sh
+++ shell/k8s.sh
@@ -3,3 +3,6 @@ kubectl -n namesapcea get secrets/key -o json | jq '.namespace = "default"' | ku
 
 # delete all object in namespace
 kubectl -n ${namespace} delete daemonsets,replicasets,services,deployments,pods,rc --all
+
+# remove all images 
+docker rmi -f $(docker images -a -q)
