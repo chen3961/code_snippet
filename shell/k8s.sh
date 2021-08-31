462934cfd126c16d00715c7bb888871fa7cef3b8
chen3961 <chen3961@qq.com>
2021-08-31 14:46:31 +0800

    Update k8s.sh

--- shell/k8s.sh
+++ shell/k8s.sh
@@ -3,3 +3,13 @@ kubectl -n namesapcea get secrets/key -o json | jq '.namespace = "default"' | ku
 
 # delete all object in namespace
 kubectl -n ${namespace} delete daemonsets,replicasets,services,deployments,pods,rc --all
+
+# check veth for the pod - mainly for dump network packet for pod
+# in pod
+cat /sys/class/net/eth0/iflink
+# it will get a number such as “23”
+# in host
+ip link | grep ^{number}
+# get the veth id
+
+
